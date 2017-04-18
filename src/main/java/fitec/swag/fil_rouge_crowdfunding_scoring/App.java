802335b41fa2fdc683ffebf5483901c02084bb74
Fitec <Fitec@HPG3-006>
2017-04-18 15:26:58 +0200

    récupère le json des 200 premières pages

--- src/main/java/fitec/swag/fil_rouge_crowdfunding_scoring/App.java
+++ src/main/java/fitec/swag/fil_rouge_crowdfunding_scoring/App.java
@@ -5,14 +5,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.DataNode;
@@ -28,11 +28,12 @@ public class App {
 	public static void main(String[] args) {
 		// Validate.isTrue(args.length == 1, "usage: supply url to fetch");
 		// String url = args[0];
+		String url = "https://www.kickstarter.com/discover/advanced?woe_id=0&sort=newest&seed=2487379&page=";
 
 		// get urls of all projects from the first page (search with empty query
 		// and order by newest)
 
-		getJSON1();
+		getJSON1(url);
 
 	}
 
@@ -40,123 +41,46 @@ public class App {
 	 * Récupère les urls des projets depuis la page des résultats de recherche
 	 * puis requête la page du projet et récupère le JSON dessus
 	 */
-	public static void getJSON1() {
-		try {
-			// récupération des urls des 20 premiers projets
-			String url = "https://www.kickstarter.com/discover/advanced?sort=newest";
-			Document doc;
-			JSONObject jsonObject = null;
-			doc = Jsoup.connect(url).get();
-			Elements scriptTags = doc.getElementsByClass("project-thumbnail-wrap");
+	public static void getJSON1(String baseUrl) {
+		int nbPage = 15;
+		while (true) {
+			String url = baseUrl + nbPage;
+			try {
+				// récupération des urls des 20 premiers projets
+				// String url =
+				// "https://www.kickstarter.com/discover/advanced?ref=discovery_overlay";
+				Document doc;
+				doc = Jsoup.connect(url).get();
+				Elements scriptTags = doc.getElementsByClass("project-thumbnail-wrap");
 
-			// récupération du JSON contenant les infos de chaque projet
-			Collection<JSONObject> collection = new ArrayList<>();
-			for (int i = 0; i < scriptTags.size(); i++) {
-				Element element = scriptTags.get(i);
-				String urlProjet = "https://www.kickstarter.com" + element.attr("href");
-				collection.add(buildJSONObject(urlProjet));
-				String string = "\"";
+				// récupération du JSON contenant les infos de chaque projet
+				Collection<JSONObject> collection = new ArrayList<>();
+				for (int i = 0; i < scriptTags.size(); i++) {
+					Element element = scriptTags.get(i);
+					String urlProjet = "https://www.kickstarter.com" + element.attr("href");
+					collection.add(buildJSONObject(urlProjet));
+				}
+
+				// constitution du JSON
+				JSONObject result = new JSONObject();
+				result.put("url", url);
+				result.put("results", collection);
+				result.put("date", new Date().toString());
+
+				String fileName = "kickstarter.JsonBase." + getFormattedDate() + ".page" + nbPage;
+				FileUtils.write(new File(fileName), result.toString(), StandardCharsets.UTF_8);
+				nbPage++;
+			} catch (Exception e) {
+				e.printStackTrace();
+				break;
 			}
-			FileUtils.write(new File("output.json"), new JSONArray(collection).toString(), StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * Récupère les données des projets depuis la page résultat de recherche
-	 */
-	public static void getJSON2() {
-		try {
-			// récupération des urls des 20 premiers projets
-			String url = "https://www.kickstarter.com/discover/advanced?sort=newest";
-			Document doc;
-			JSONObject jsonObject = null;
-			doc = Jsoup.connect(url).get();
-			Elements scriptTags = doc.getElementsByClass("project-thumbnail-wrap");
-
-			// récupération du JSON contenant les infos de chaque projet
-			Collection<JSONObject> collection = new ArrayList<>();
-			for (int i = 0; i < scriptTags.size(); i++) {
-				Element element = scriptTags.get(i);
-				String urlProjet = "https://www.kickstarter.com" + element.attr("href");
-				collection.add(buildJSONObject(urlProjet));
-				String string = "\"";
-			}
-			FileUtils.write(new File("output.json"), new JSONArray(collection).toString(), StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private static void buildPropsMapFromJSON(String url) throws IOException {
-		// Document doc;
-		// doc = Jsoup.connect(url).get();
-		Map<String, Object> props = new HashMap<>();
-		JSONObject jsonObject = buildJSONObject(url);
-
-		// id
-		props.put(CONSTANTS.META_PROJECT_ID, jsonObject.getInt("id"));
-
-		// name
-		props.put(CONSTANTS.META_NAME, jsonObject.getString("name"));
-		// Elements imports = doc.select("meta[property='og:title']");
-		// if (!imports.isEmpty()) {
-		// props.put(CONSTANTS.META_NAME, imports.get(0).attr("content"));
-		// } else {
-		// System.out.println("coucou");
-		// }
-		// url
-		props.put(CONSTANTS.META_URL, jsonObject.getJSONObject("urls").getJSONObject("web").getString("project"));
-
-		// category
-		props.put(CONSTANTS.META_CATEGORY, jsonObject.getJSONObject("category").getString("name"));
-
-		// subcategory
-		props.put(CONSTANTS.META_SUBCATEGORY, null);
-
-		// location
-		props.put(CONSTANTS.META_LOCATION, jsonObject.getJSONObject("location").getString("short_name"));
-
-		// status
-		props.put(CONSTANTS.META_STATUS, jsonObject.getString("state"));
-
-		// goal
-		props.put(CONSTANTS.META_GOAL, jsonObject.getInt("goal"));
-
-		// pledged
-		props.put(CONSTANTS.META_PLEDGED, jsonObject.getInt("pledged"));
-
-		// funded percentage
-		props.put(CONSTANTS.META_FUNDED_PERCENTAGE, null);
-
-		// backers
-		props.put(CONSTANTS.META_BACKERS, jsonObject.getInt("backers_count"));
-
-		// funded date
-		props.put(CONSTANTS.META_FUNDED_DATE, null);
-
-		// levels
-		props.put(CONSTANTS.META_LEVELS, jsonObject.getJSONArray("rewards").length());
-
-		// reward level
-		props.put(CONSTANTS.META_REWARD_LEVELS, null);
-
-		// updates
-		props.put(CONSTANTS.META_UPDATES, jsonObject.getInt("updates_count"));
-
-		// comments
-		props.put(CONSTANTS.META_COMMENTS, jsonObject.getInt("comments_count"));
-
-		// duration
-		props.put(CONSTANTS.META_SUBCATEGORY, null);
-
-		// System.out.println(props);
-
-		JSONObject jsonObject2 = new JSONObject(props);
-		System.out.println(jsonObject2.toString());
+	private static String getFormattedDate() {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+		return format1.format(cal.getTime());
 	}
 
 	public static JSONObject buildJSONObject(String url) throws IOException {
@@ -178,6 +102,7 @@ public class App {
 						// System.out.println(jsonObject.keySet());
 					}
 				} while (line != null);
+				reader.close();
 			}
 		}
 
